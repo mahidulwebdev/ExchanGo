a443e19ec4ada3e1d298394eabf63251b80d95b1
Mahidul Hasan <145708224+mahidulwebdev@users.noreply.github.com>
2024-08-01 17:09:14 +0600

    fixed a little  bug in customHook

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -1,9 +1,9 @@
-import { useState, useEffectuseCurrency.js } from "react";
+import { useState, useEffect } from "react";
 
 const useCurrencyRate = (currency) => {
   const [currencyRate, setRate] = useState([]);
   useEffect(() => {
-    fetch(`http://www.floatrates.com/daily/usd.json`)
+    fetch(`http://www.floatrates.com/daily/${currency}.json`)
       .then((res) => res.json())
       .then((result) => setRate(result));
   }, [currency]);
